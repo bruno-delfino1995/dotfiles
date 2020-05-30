21fbbb93fb2c421295f4064b14445b68429912e4
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2020-05-29 22:14:36 -0300

    doom: disable completion, syntax, linter, ... helpers

--- desktop/.doom.d/init.el
+++ desktop/.doom.d/init.el
@@ -76,8 +76,8 @@
        vterm             ; the best terminal emulation in Emacs
 
        :checkers
-       syntax              ; tasing you for every semicolon you forget
-       spell             ; tasing you for misspelling mispelling
+       ;; syntax              ; tasing you for every semicolon you forget
+       ;; spell             ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
@@ -90,7 +90,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       lsp
+       ;; lsp
        ;;macos             ; MacOS-specific commands
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
